c79802d5d3e204e27aa8036e4b97cd56f71200e8
Marcus V <begheli2020@gmail.com>
2022-04-04 13:47:01 -0300

    finalizado buscando chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,112 @@
 import './dashboard.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Header from "../../components/Header";
 import Title from '../../components/Title';
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+
+import firebase from '../../services/firebaseConnection';
+// import { update } from 'tar';
+
+const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
 
 export default function Dashboard() {
-    const [chamados, setChamados] = useState(1);
+    const [chamados, setChamados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+
+    useEffect(() => {
+
+        loadChamados();
+
+        return () => {
+
+        }
+
+    }, []);
+
+    async function loadChamados() {
+      await listRef.limit(5)
+      .get()
+      .then((snapshot)=>{
+          updateState(snapshot)
+      })
+      .catch((error)=>{
+        console.log('Deu algum erro: ', error);
+        setLoadingMore(false);
+      })
+
+      setLoading(false);
+    }
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if (!isCollectionEmpty) {
+            let lista = [];
+
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clientId: doc.data().clientId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+                })
+            })
+
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1]; //pegando o ultimo documento buscado.
+
+            setChamados(chamados => [...chamados, ...lista]);
+            setLastDocs(lastDoc);
+
+        } else {
+            setIsEmpty(true);
+        }
+
+        setLoadingMore(false);
+
+    }
+
+    async function handleMore(){
+        setLoadingMore(true);
+        await listRef.startAfter(lastDocs).limit(5)
+        .get()
+        .then((snapshot)=>{
+            updateState(snapshot)
+        })
+    }
+
+    function togglePostModal(item){
+        console.log(item)
+    }
+
+    if (loading) {
+        return (
+            <div>
+                <Header />
+
+                <div className="content">
+                    <Title name="Atendimentos">
+                        <FiMessageSquare size={25} />
+                    </Title>
+
+                    <div className="container dashboard">
+                        <span>Buscando chamados...</span>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+
 
     return (
         <div>
@@ -45,24 +144,32 @@ export default function Dashboard() {
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td data-label="Cliente">Sujeito</td>
-                                    <td data-label="Assunto">Suporte</td>
-                                    <td data-label="Status">
-                                        <span className="badge" style={{backgroundColor: '#5CB85C'}}>Em aberto</span>
-                                    </td>
-                                    <td data-label="Cadastrado">31/03/2022</td>
-                                    <td data-label="#">
-                                        <button className="action" style={{backgroundColor: '#3583F6'}}>
-                                            <FiSearch color="#FFF" size={17} />
-                                        </button>
-                                        <button className="action" style={{backgroundColor: '#F6A935'}}>
-                                            <FiEdit2 color="#FFF" size={17} />
-                                        </button>
-                                    </td>
-                                </tr>
+                                {chamados.map((item, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td data-label="Cliente">{item.cliente}</td>
+                                            <td data-label="Assunto">{item.assunto}</td>
+                                            <td data-label="Status">
+                                                <span className="badge" style={{ backgroundColor: item.status === 'Aberto' ? '#5CB85C' : '#999' }}>{item.status}</span>
+                                            </td>
+                                            <td data-label="Cadastrado">{item.createdFormated}</td>
+                                            <td data-label="#">
+                                                <button className="action" style={{ backgroundColor: '#3583F6' }} onClick={()=> togglePostModal(item)}>
+                                                    <FiSearch color="#FFF" size={17} />
+                                                </button>
+                                                <button className="action" style={{ backgroundColor: '#F6A935' }}>
+                                                    <FiEdit2 color="#FFF" size={17} />
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
                             </tbody>
                         </table>
+
+                        {loadingMore && <h3 style={{textAlign: 'center', marginTop: 15 }}>Buscando dados...</h3>}
+                        { !loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
+
                     </>
                 )}
 
--- src/pages/Newcall/index.js
+++ src/pages/Newcall/index.js
@@ -1,14 +1,101 @@
-import './newcall.css';
+import { useState, useEffect, useContext } from 'react';
+
+import firebase from '../../services/firebaseConnection';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import { AuthContext } from '../../contexts/auth';
+import { toast } from 'react-toastify'
 
 import { FiPlusCircle } from 'react-icons/fi'
+import './newcall.css';
 
 export default function Newcall() {
 
-    function handleRegister(e){
+    const [loadCustomers, setLoadCustomers] = useState(true);
+    const [customers, setCustomers] = useState([]);
+    const [customerSelected, setCustomerSelected] = useState(0);
+
+
+
+    const [assunto, setAssunto] = useState('Suporte');
+    const [status, setStatus] = useState('Aberto')
+    const [complemento, setComplemento] = useState('');
+
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+
+        async function loadCustomers() {
+            await firebase.firestore().collection('customers')
+                .get()
+                .then((snapshot) => {
+                    let lista = []
+
+                    snapshot.forEach((doc) => {
+                        lista.push({
+                            id: doc.id,
+                            nomeFantasia: doc.data().nomeFantasia
+                        })
+                    })
+
+                    if (lista.length === 0) {
+                        console.log('Nenhuma empresa encontrada.')
+                        setCustomers([{ id: '1', nomeFantasia: 'freela' }]);
+                        setLoadCustomers(false);
+                        return;
+                    }
+
+                    setCustomers(lista);
+                    setLoadCustomers(false);
+
+                })
+                .cacth((error) => {
+                    console.log('Deu algum erro', error);
+                    setLoadCustomers(false);
+                    setCustomers([{ id: '1', nomeFantasia: '' }]);
+                })
+        }
+
+        loadCustomers();
+
+    }, [])
+
+    async function handleRegister(e) {
         e.preventDefault();
-        alert('testeeee')
+       
+        await firebase.firestore().collection('chamados')
+        .add({
+            created: new Date(),
+            cliente: customers[customerSelected].nomeFantasia,
+            clienteId: customers[customerSelected].id,
+            assunto: assunto,
+            status: status,
+            complemento: complemento,
+            userId: user.uid
+        })
+        .then(()=>{
+            toast.success('Chamado criado com sucesso!')
+            setComplemento('');
+            setCustomerSelected(0);
+        })
+        .cacth((error)=>{
+            toast.error('Ops, erro ao registrar, tente mais tarde.')
+            console.log(error);
+        })
+    }
+
+    function handleChangeSelect(e) { //chamado quando troca o assunto
+        setAssunto(e.target.value);
+    }
+
+    function handleOptionChange(e) { //chamado quando troca o status
+        setStatus(e.target.value);
+    }
+
+    function handleChangeCustomers(e) { //chamado quando troca de cliente
+        console.log('index do cliente selecionado: ', e.target.value);
+        console.log('cliente selecionado ', customers[e.target.value]);
+        setCustomerSelected(e.target.value);
     }
 
     return (
@@ -25,17 +112,26 @@ export default function Newcall() {
                     <form className="form-profile" onSubmit={handleRegister}>
 
                         <label>Cliente</label>
-                        <select>
-                            <option key={1} value={1}>
-                                Sujeito programador
-                            </option>
-                        </select>
+
+                        {loadCustomers ? (
+                            <input type="text" disabled={true} value="Carregando clientes..." />
+                        ) : (
+                            <select value={customerSelected} onChange={handleChangeCustomers}>
+                                {customers.map((item, index) => {
+                                    return (
+                                        <option key={item.id} value={index}>
+                                            {item.nomeFantasia}
+                                        </option>
+                                    )
+                                })}
+                            </select>
+                        )}
 
                         <label>Assunto</label>
-                        <select>
+                        <select value={assunto} onChange={handleChangeSelect}>
                             <option value="Suporte">Suporte</option>
                             <option value="Visita Técnica">Visita Técnica</option>
-                            <option value="Finaneiro">Finaneiro</option>
+                            <option value="Financeiro">Financeiro</option>
                         </select>
 
                         <label>Status</label>
@@ -44,6 +140,8 @@ export default function Newcall() {
                                 type="radio"
                                 name="radio"
                                 value="Aberto"
+                                onChange={handleOptionChange}
+                                checked={status === 'Aberto'}
                             />
                             <span>Em aberto</span>
 
@@ -51,6 +149,8 @@ export default function Newcall() {
                                 type="radio"
                                 name="radio"
                                 value="Progresso"
+                                onChange={handleOptionChange}
+                                checked={status === 'Progresso'}
                             />
                             <span>Progresso</span>
 
@@ -58,14 +158,18 @@ export default function Newcall() {
                                 type="radio"
                                 name="radio"
                                 value="Atendido"
+                                onChange={handleOptionChange}
+                                checked={status === 'Atendido'}
                             />
                             <span>Atendido</span>
                         </div>
 
                         <label className="complemento">Complemento</label>
                         <textarea
-                        type="text"
-                        placeholder="Descreva seu problema (opcional)." 
+                            type="text"
+                            placeholder="Descreva seu problema (opcional)."
+                            value={complemento}
+                            onChange={(e) => setComplemento(e.target.value)}
                         />
 
                         <button type="submit">Registrar</button>
